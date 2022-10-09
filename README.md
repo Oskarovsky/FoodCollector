04fa5db9d6355ff906290d5eb5e7cd315f41cca4
Oskar Slyk <oskar.slyk@mavoco.com>
2022-10-10 00:13:25 +0200

    API provider

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # FoodCollector
+
+
+https://blog.logrocket.com/how-to-build-a-rest-api-with-golang-using-gin-and-gorm/
+
+
+https://medium.com/@sohamkamani/adding-a-database-to-a-go-web-application-b0e8e8b16fb9
+
+https://tutorialedge.net/golang/consuming-restful-api-with-go/
+
+
+https://www.programmableweb.com/category/nutrition/api
--- main.go
+++ main.go
@@ -9,21 +9,17 @@ import (
 	"net/http"
 )
 
-func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome DJ!")
-}
-
 type allProducts []models.Product
 
-func handleRequest() {
+func handleRequest() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homeLink)
 	router.HandleFunc("/product", handlers.CreateProduct).Methods(http.MethodPost)
 	router.HandleFunc("/product", handlers.GetAllProducts).Methods(http.MethodGet)
 	router.HandleFunc("/product/{id}", handlers.GetOneProduct).Methods(http.MethodGet)
 	router.HandleFunc("/product/{id}", handlers.UpdateProduct).Methods(http.MethodPatch)
 	router.HandleFunc("/product/{id}", handlers.DeleteProduct).Methods(http.MethodDelete)
 	log.Fatal(http.ListenAndServe("localhost:8080", router))
+	return router
 }
 
 func main() {
